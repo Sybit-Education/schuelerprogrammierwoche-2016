688a43d1e8859fde025153ec3b4ead463679733d
Korherr <kkr@sybit.de>
2016-08-03 15:51:59 +0200

    Fix Service

--- src/main/java/com/sybit/education/taschengeldboerse/service/SchuelerServiceImpl.java
+++ src/main/java/com/sybit/education/taschengeldboerse/service/SchuelerServiceImpl.java
@@ -3,10 +3,12 @@ package com.sybit.education.taschengeldboerse.service;
 import com.sybit.education.taschengeldboerse.domain.Schueler;
 import com.sybit.education.taschengeldboerse.repository.SchuelerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by kkr on 03.08.2016.
  */
+@Service
 public class SchuelerServiceImpl implements SchuelerService {
 
     @Autowired
