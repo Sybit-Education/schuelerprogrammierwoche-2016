4b11ac7a8e699dd462d42ddddda2a061f773dd12
unknown <Schulungsnb03@SCHULUNGSNB03.sybit.de>
2016-08-03 14:22:29 +0200

    tabelle bewerbung angelegt

---
+++ src/main/resources/db/migration/V1_6__jobbewerbung.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `taschengeldboerse`.`jobbewerbung` (
+  `id` INT NULL AUTO_INCREMENT,
+  `schuelerid` INT NULL,
+  `jobid` INT NULL,
+  `timestamp` TIMESTAMP NULL,
+  PRIMARY KEY (`id`));
