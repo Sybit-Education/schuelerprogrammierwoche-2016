f86ebf5bde412ef6238dccac1566b718edbbc067
Florian Renz <Florian.Renz@sybit.de>
2016-08-04 10:32:31 +0200

    Add anbieter profil

--- src/main/java/com/sybit/education/taschengeldboerse/repository/SchuelerRepository.java
+++ src/main/java/com/sybit/education/taschengeldboerse/repository/SchuelerRepository.java
@@ -2,10 +2,8 @@ package com.sybit.education.taschengeldboerse.repository;
 
 
 import com.sybit.education.taschengeldboerse.domain.Schueler;
-import com.sybit.education.taschengeldboerse.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 /**
  * Created by stl on 09.06.2015.
---
+++ src/main/java/com/sybit/education/taschengeldboerse/service/AnbieterServiceImpl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sybit.education.taschengeldboerse.service;
+
+import com.sybit.education.taschengeldboerse.domain.Anbieter;
+import com.sybit.education.taschengeldboerse.repository.AnbieterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author frz
+ */
+public class AnbieterServiceImpl implements AnbieterService {
+    
+    @Autowired
+    AnbieterRepository anbieterRepository;
+    
+    @Override
+    public Anbieter findAnbieterById(int id){
+        return anbieterRepository.findById(id);
+    }
+    
+}
